c56a7b0163561032f882b5ba8dae87b531a3e2f8
Saulo Victor <saulovictor@hotmail.com.br>
2020-09-14 23:06:57 -0300

    Add files via upload

---
+++ Resize.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Resize_and_Aspect_Ratio
+{
+    partial class Resize
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ProportionalPanel = new System.Windows.Forms.Panel();
+            this.AnchorPanel = new System.Windows.Forms.Panel();
+            this.AnchorPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ProportionalPanel
+            // 
+            this.ProportionalPanel.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ProportionalPanel.Location = new System.Drawing.Point(12, 12);
+            this.ProportionalPanel.Name = "ProportionalPanel";
+            this.ProportionalPanel.Size = new System.Drawing.Size(476, 328);
+            this.ProportionalPanel.TabIndex = 0;
+            // 
+            // AnchorPanel
+            // 
+            this.AnchorPanel.BackColor = System.Drawing.Color.LightBlue;
+            this.AnchorPanel.Controls.Add(this.ProportionalPanel);
+            this.AnchorPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.AnchorPanel.Location = new System.Drawing.Point(0, 0);
+            this.AnchorPanel.Name = "AnchorPanel";
+            this.AnchorPanel.Size = new System.Drawing.Size(500, 350);
+            this.AnchorPanel.TabIndex = 1;
+            this.AnchorPanel.SizeChanged += new System.EventHandler(this.AnchorPanel_SizeChanged);
+            // 
+            // Resize
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 350);
+            this.Controls.Add(this.AnchorPanel);
+            this.Name = "Resize";
+            this.Text = "Aspect Ratio";
+            this.AnchorPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel ProportionalPanel;
+        private System.Windows.Forms.Panel AnchorPanel;
+    }
+}
---
+++ Resize.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Resize_and_Aspect_Ratio
+{
+    public partial class Resize : Form
+    {
+        //The anchor panel must be docked as a Fill
+        //The ProportionalPanel is anchored as Top Left
+        public Resize()
+        {
+            InitializeComponent();
+        }
+
+        private void AnchorPanel_SizeChanged(object sender, EventArgs e)
+        {
+            //Calculates the new size of the panel
+            //You can use any proportion here
+            int Height = (int)(AnchorPanel.Width * 0.71);
+            int Width = (int)(AnchorPanel.Height / 0.71);
+
+            //one of the two sides needs to be matched to the proportions
+            if (AnchorPanel.Width <= AnchorPanel.Height / 0.71)
+            {
+                //Here you can setup some margins (i'm using 25px for each side)
+                //If you don't wnat any margins just remove the number
+                ProportionalPanel.Height = Height - 25;
+                ProportionalPanel.Width = AnchorPanel.Width - 25;
+
+            }
+            else
+            {
+                ProportionalPanel.Width = Width - 25;
+                ProportionalPanel.Height = AnchorPanel.Height - 25;
+            }
+        }
+    }
+}
